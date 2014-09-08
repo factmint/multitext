6f47a709e8f51be9aa7fe87ddc2507d4a60215ef
Benjamin Brown <ben.brown@factmint.com>
2014-09-08 13:56:47 +0100

    Initial commit

---
+++ dist/multitext.js
@@ -0,0 +1,11 @@
+Snap.plugin(function (Snap, Element, Paper, glob) {
+    Paper.prototype.multitext = function (x, y, text) {
+        text = text.split("\n");
+        var multitext = this.text(x, y, text);
+        multitext.selectAll("tspan:nth-child(n+2)").attr({
+            dy: "1.8em",
+            x: x
+        });
+        return multitext;
+    };
+});
