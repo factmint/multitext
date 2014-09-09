f242b1f49cec0aa827a964d5db775c9f5b46a50f
Benjamin Brown <ben.brown@factmint.com>
2014-09-09 14:08:42 +0100

    Wrapped as a RequireJS AMD module

--- dist/multitext.js
+++ dist/multitext.js
@@ -1,11 +1,13 @@
-Snap.plugin(function (Snap, Element, Paper, glob) {
-    Paper.prototype.multitext = function (x, y, text) {
-        text = text.split("\n");
-        var multitext = this.text(x, y, text);
-        multitext.selectAll("tspan:nth-child(n+2)").attr({
-            dy: "1.8em",
-            x: x
-        });
-        return multitext;
-    };
+define(function() {
+	return Snap.plugin(function (Snap, Element, Paper, glob) {
+	    Paper.prototype.multitext = function (x, y, text) {
+	        text = text.split("\n");
+	        var multitext = this.text(x, y, text);
+	        multitext.selectAll("tspan:nth-child(n+2)").attr({
+	            dy: "1.8em",
+	            x: x
+	        });
+	        return multitext;
+	    };
+	});
 });
